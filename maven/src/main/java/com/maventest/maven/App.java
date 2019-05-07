84211fbf07b003dca7dfd99ab3e4f2fb290b2f37
estevewu <estevewu@sina.com>
2019-05-07 14:51:32 +0800

    new2

--- maven/src/main/java/com/maventest/maven/App.java
+++ maven/src/main/java/com/maventest/maven/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! My Git! next OK!!!" );
-        System.out.println( "New!!!" );
+        System.out.println( "New1!!!" );
+        System.out.println( "New2!!!" );
     }
 }
